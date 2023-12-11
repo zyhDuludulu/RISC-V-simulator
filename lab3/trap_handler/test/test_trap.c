181e6135377ab12a40fdd2ec2fa7752802c2d70d
zyhDuludulu <101815207+zyhDuludulu@users.noreply.github.com>
2023-12-11 20:36:08 +0800

    test case: test_trap.c

---
+++ lab3/trap_handler/test/test_trap.c
@@ -0,0 +1,26 @@
+#include "lib.h"
+int main() {
+    int a = 10, b = 20, c = 30, d = 12345, e = 1, f = 2;
+    int x = a + b;
+    print_d(x);
+    print_c('\n');
+    int y = a - b;
+    print_d(y);
+    print_c('\n');
+    int z = c * d;
+    print_d(z);
+    print_c('\n');
+    print_s("trap begins here\n");
+    syscall();
+    print_s("trap ends here\n");
+    x = d / c;
+    print_d(x);
+    print_c('\n');
+    y = d << f;
+    print_d(y); 
+    print_c('\n');
+    z = d >> 4;
+    print_d(z);
+    print_c('\n');
+    exit_proc();
+}
